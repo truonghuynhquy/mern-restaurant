55389f6836885ced7cc02047ebda0ef5e5e3849c
Truong Huynh Quy <quytruong2509@gmail.com>
2024-05-06 10:38:46 +0700

    Added app server db env

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { dbConnection } from "./database/dbConnection.js";
+
+const app = express();
+dotenv.config({ path: "./config/config.env" });
+
+app.use(
+    cors({
+        origin: [process.env.FRONTEND_URL],
+        methods: ["POST"],
+        credentials: true,
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+dbConnection();
+
+export default app;
