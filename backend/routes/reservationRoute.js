2a3a83b86d4da7c63eb3e25c4bbe737c4095ce74
Truong Huynh Quy <quytruong2509@gmail.com>
2024-05-10 11:00:42 +0700

    Remove 'be' directory from 'fe' branch

--- backend/routes/reservationRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import send_reservation from "../controller/reservationController.js";
-
-const router = express.Router();
-
-router.post("/send", send_reservation);
-
-export default router;
